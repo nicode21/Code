81ac39721a91ddda00f2a07f6eaf9dce5dab1932
nicode21 <nijatan@code.edu.az>
2022-10-01 18:56:59 +0400

    add files

---
+++ HomeTaskSade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeTaskSade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 24;
+
+            int count = 0;
+
+            
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n%i==0) {
+                    count++;
+                }
+                
+            }
+            //if (count == 2) { Console.WriteLine("Sade ededdir");
+            //}
+            //else { Console.WriteLine("Murekkeb ededdir"); 
+            //}
+
+           string result =count == 2 ? "sade ededdir" : "murekkeb ededdir";
+
+            Console.WriteLine(result);
+        }
+    }
+}
